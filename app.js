721252bcd833ea4e0433c5a67995bab96c3d5c44
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2024-03-19 10:20:16 +0100

    refactoring conditional statements into functions

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ const showSuccess = (input) => {
 
 const isValidEmail = (input) => {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(input).toLowerCase())
+
+  if (re.test(input.value.trim())) {
+    showSuccess(input)
+  } else {
+    showError(input, "Email is not correct")
+  }
 }
 
 const checkRequired = (array) => {
@@ -31,9 +36,27 @@ const checkRequired = (array) => {
   })
 }
 
-// trim and uppercase id
+// get field name by id
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} carachters`)
+  } else if (inpput.value.length > max) {
+    showError(input, `${getFieldName(input)} must be at less than ${max} carachters`)
+  } else {
+    showSuccess(success)
+  }
+}
+
+const checkPsw = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password do not match")
+  } else {
+    showSuccess(input)
+  }
 }
 
 //Events
@@ -41,4 +64,8 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
 
   checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15)
+  isValidEmail(email)
+  checkLength(password, 6, 25)
+  checkPsw(password, password2)
 })
